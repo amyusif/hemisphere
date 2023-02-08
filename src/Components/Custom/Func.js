ad5fbdb72e8bd6a2dba8879c0b1e0e5bfe1db00a
Yusif <am.yusif530@gmail.com>
2023-02-07 19:10:37 -0800

    changed

--- src/Components/Custom/Func.js
+++ src/Components/Custom/Func.js
@@ -2,24 +2,28 @@ import React from "react";
 import { useState } from "react";
 
 const Func = () => {
-  const [location, setLocation] = useState({ latitude: 20, longitude: 30 });
+  const [location, setLocation] = useState({ latitude: 20, errorMessage: " " });
 
-  window.navigator.geolocation.getCurrentPosition((position) => {
-    setLocation({
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
-  });
-  return (
-    <>
-      <div>
-        <h3>The latitude of the current location is : {location.latitude}</h3>
-      </div>
-      <div>
-        <h3>The longitude of the current location is : {location.longitude}</h3>
-      </div>
-    </>
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      setLocation({
+        latitude: position.coords.latitude,
+      });
+    },
+    (error) => {
+      setLocation({ errorMessage: error.message });
+    }
   );
+  if (location.latitude && !location.errorMessage) {
+      return <div>{ location.latitude }</div>
+  }
+  if (!location.latitude && location.errorMessage) {
+    return <div>{ location.errorMessage }</div>
+}
+else{
+  return <div>Loading.......</div>
+}
+ 
 };
 
 export default Func;
